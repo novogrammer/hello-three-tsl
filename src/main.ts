37d8bf0f502b2af6af4543369ea464726f8bf3eb
Yusuke Kawamoto <novogrammer@gmail.com>
2024-07-25 15:32:44 +0900

    npm i three

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,73 @@
+import { getElementSize } from './dom_utils';
 import './style.scss'
+import * as THREE from "three";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<section>Hello!</section>`;
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<section class="p-section-first"></section>`;
+
+
+const sectionFirst=document.querySelector<HTMLHtmlElement>(".p-section-first");
+
+if(!sectionFirst){
+  throw new Error("sectionFirst is null");
+}
+
+const {width,height}=getElementSize(sectionFirst);
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( width, height );
+renderer.setAnimationLoop( animate );
+sectionFirst.appendChild( renderer.domElement );
+
+
+
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+
+{
+  const ambientLight=new THREE.AmbientLight(0xffffff,0.6);
+  scene.add(ambientLight);
+}
+{
+  const directionalLight=new THREE.DirectionalLight(0xffffff,2);
+  directionalLight.position.set(10,10,10);
+  scene.add(directionalLight);
+}
+
+
+window.addEventListener("resize",()=>{
+  onResize();
+})
+onResize();
+
+function onResize(){
+  if(!sectionFirst){
+    throw new Error("sectionFirst is null");
+  }
+  const {width,height}=getElementSize(sectionFirst);
+  renderer.setSize(width,height);
+  camera.aspect=width/height;
+  camera.updateProjectionMatrix();
+}
+
+function animate() {
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render( scene, camera );
+
+}
+
+
 
