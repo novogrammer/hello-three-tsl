cac821e190d7e78c31dc3f001d53844dfcc8ae25
Yusuke Kawamoto <novogrammer@gmail.com>
2024-07-26 14:34:15 +0900

    TSLを色々試す。

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { getElementSize } from './dom_utils';
 import './style.scss'
 import * as THREE from "three";
 import WebGPURenderer from 'three/examples/jsm/renderers/webgpu/WebGPURenderer.js';
-import { MeshStandardNodeMaterial, vec4 } from 'three/examples/jsm/nodes/Nodes.js';
+import { MeshStandardNodeMaterial, timerGlobal, tslFn, float,uv,vec4, positionLocal, texture } from 'three/examples/jsm/nodes/Nodes.js';
+
+/// <reference path="./three-more.d.ts" />
+import { loop/*,Break,Continue*/ } from 'three/examples/jsm/nodes/utils/LoopNode.js';
+
+
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<section class="p-section-first"></section>`;
@@ -26,6 +31,11 @@ renderer.setAnimationLoop( animate );
 sectionFirst.appendChild( renderer.domElement );
 
 
+const textureLoader = new THREE.TextureLoader();
+
+const uvTexture = textureLoader.load( './uv_grid_opengl.jpg' );
+uvTexture.wrapS = THREE.RepeatWrapping;
+uvTexture.wrapT = THREE.RepeatWrapping;
 
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -33,8 +43,26 @@ const material = new MeshStandardNodeMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-const colorNode=vec4(1,0,0,1);
-material.colorNode=colorNode;
+function makeMyColorNode(){
+  const changingColor=tslFn(([base=vec4(1,1,1,1),green=float(0),blue=float(0)])=>{
+    const total = float( 0 ).toVar();
+    loop({start:0,end:2},()=>{
+      total.addAssign(float(.5));
+    });
+  
+  
+    return base.mul(vec4(total,green,blue,1));
+  });
+  
+  const base=texture(uvTexture,uv().mul(1.0));
+  const blue=timerGlobal(1).mul(Math.PI*2).sin().mul(0.5).add(0.5);
+  const green=positionLocal.y;
+  // material.positionNode=vec3(positionLocal.x,0,positionLocal.z)
+    return changingColor(base,green,blue);
+}
+
+
+material.colorNode=makeMyColorNode();
 
 camera.position.z = 5;
 
---
+++ src/three-more.d.ts
@@ -0,0 +1,10 @@
+declare module "three/examples/jsm/nodes/utils/LoopNode.js"{
+  export const loop: (params:{start:number,end:number}, method: ({}:{i:number}) => void) => void;
+
+  export const Break: () => void;
+  export const Continue: () => void;
+  
+}
+
+
+
