f6579ee3ae286e0b36c8d3fc022229677d5ebdb9
Yusuke Kawamoto <novogrammer@gmail.com>
2024-07-26 15:46:08 +0900

    ごちゃごちゃするのでノードを別ファイルへ

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,7 @@
 import { getElementSize } from './dom_utils';
+import { makeMyColorNode } from './my_nodes';
 import './style.scss'
-// import * as THREE from "three";
-declare module "three/webgpu"{
-  export const loop: (params:{start:number,end:number}, method: ({}:{i:number}) => void) => void;
-
-  export const Break: () => void;
-  export const Continue: () => void;
-}
 import * as THREE from 'three/webgpu';
-
-const { timerGlobal, tslFn, float,uv,vec4, positionLocal, texture,loop } =THREE;
-
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<section class="p-section-first"></section>`;
 
 
@@ -44,26 +35,10 @@ const material = new THREE.MeshStandardNodeMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-function makeMyColorNode(){
-  const changingColor=tslFn(([base=vec4(1,1,1,1),green=float(0),blue=float(0)])=>{
-    const total = float( 0 ).toVar();
-    loop({start:0,end:2},()=>{
-      total.addAssign(float(.5));
-    });
-  
-  
-    return base.mul(vec4(total,green,blue,1));
-  });
-  
-  const base=texture(uvTexture,uv().mul(1.0));
-  const blue=timerGlobal(1).mul(Math.PI*2).sin().mul(0.5).add(0.5);
-  const green=positionLocal.y;
-  // material.positionNode=vec3(positionLocal.x,0,positionLocal.z)
-    return changingColor(base,green,blue);
-}
 
 
-material.colorNode=makeMyColorNode();
+
+material.colorNode=makeMyColorNode(uvTexture);
 
 camera.position.z = 5;
 
